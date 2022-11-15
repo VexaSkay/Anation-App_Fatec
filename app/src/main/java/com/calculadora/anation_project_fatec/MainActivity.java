622f918fe10685718e5795a0a2d438374a87e6fe
Nicolas <nicoschianti@gmail.com>
2022-11-15 20:50:49 -0300

    Criação da Tela de Login (Protótipo 1, necessário correção de design ainda).

--- app/src/main/java/com/calculadora/anation_project_fatec/MainActivity.java
+++ app/src/main/java/com/calculadora/anation_project_fatec/MainActivity.java
@@ -3,6 +3,11 @@ package com.calculadora.anation_project_fatec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView username =(TextView) findViewById(R.id.usuario);
+        TextView password =(TextView) findViewById(R.id.senha);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.login_btn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correto
+                    Toast.makeText(MainActivity.this, "Login realizado",Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(MainActivity.this, "Login falhou",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
