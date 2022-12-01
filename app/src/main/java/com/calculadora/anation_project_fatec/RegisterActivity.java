f0933c6c04e230203f97437a980e4d39bdcb1e17
Nicolas <nicoschianti@gmail.com>
2022-12-01 15:33:31 -0300

    Tela de Registro para CNPJ

--- app/src/main/java/com/calculadora/anation_project_fatec/RegisterActivity.java
+++ app/src/main/java/com/calculadora/anation_project_fatec/RegisterActivity.java
@@ -3,6 +3,10 @@ package com.calculadora.anation_project_fatec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        EditText registeremail = (EditText) findViewById(R.id.register_email);
+        MaterialButton registerbtn = (MaterialButton) findViewById(R.id.register_btn);
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
